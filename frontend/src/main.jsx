8ae91c7e418e16cebb8077659760813e35101c51
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2022-12-13 20:53:04 -0500

    clean up UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,43 +1,86 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import '@rainbow-me/rainbowkit/styles.css';
 import {
-  getDefaultWallets,
+  // getDefaultWallets,
+  // Chain,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
+// import { Chain } from 'wagmi/chains';
 import {
   configureChains,
   createClient,
   WagmiConfig,
 } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { injectedWallet, metaMaskWallet } from '@rainbow-me/rainbowkit/wallets';
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+
 
 /* create configuration for Ethermint testnet */
 const ethermint = {
   id: 9000,
-  name: 'Evmos Testnet',
-  network: 'Evmos Testnet',
+  name: 'Ethermint',
+  network: 'ethermint',
   nativeCurrency: {
     decimals: 18,
-    name: 'Evmos Testnet',
-    symbol: 'tEVMOS',
+    name: 'Ethermint',
+    symbol: 'EMINT',
   },
   rpcUrls: {
-    default: 'http://159.65.252.178:8545/'
-  }
+    default: {
+      http: ['http://159.65.252.178:8545/'],
+    },
+  },
+  testnet: true,
 };
 
-const { chains, provider } = configureChains(
+const { provider, chains } = configureChains(
   [ethermint],
-  [publicProvider()]
+  [
+    jsonRpcProvider({
+      rpc: chain => ({ http: chain.rpcUrls.default.http[0] }),
+    }),
+  ]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'Celestia App',
-  chains
-});
+/* DEBUG */
+// const avalancheChain = {
+//   id: 43_114,
+//   name: 'Avalanche',
+//   network: 'avalanche',
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: 'Avalanche',
+//     symbol: 'AVAX',
+//   },
+//   rpcUrls: {
+//     default: {
+//       http: ['https://api.avax.network/ext/bc/C/rpc'],
+//     },
+//   },
+//   testnet: false,
+// };
+
+// const { provider, chains } = configureChains(
+//   [avalancheChain],
+//   [
+//     jsonRpcProvider({
+//       rpc: chain => ({ http: chain.rpcUrls.default.http[0] }),
+//     }),
+//   ]
+// );
+
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Recommended',
+    wallets: [
+      metaMaskWallet({ chains }),
+      injectedWallet({ chains }),
+    ],
+  },
+]);
 
 const wagmiClient = createClient({
   autoConnect: true,
@@ -52,7 +95,7 @@ const containerStyle = {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <WagmiConfig client={wagmiClient}>
-    <RainbowKitProvider chains={chains} initialChain={ethermint}>
+    <RainbowKitProvider chains={chains}>
       <div style={containerStyle}>
         <App />
       </div>
